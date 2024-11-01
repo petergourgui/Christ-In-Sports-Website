3cb054530ca5fa1e9cd25c49e2e0a2795811ed59
petergourgui <petergourgui@gmail.com>
2024-10-31 20:31:33 -0700

    Made pictures animation slower.

--- assets/js/main.js
+++ assets/js/main.js
@@ -155,5 +155,5 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  timer = setTimeout(showSlides, 4000);
+  timer = setTimeout(showSlides, 7000);
 } 
